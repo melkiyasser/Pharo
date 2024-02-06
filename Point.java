3b251931324294aac125762f23d2ee195dbc925a
YASSER <75129790+melkiyasser@users.noreply.github.com>
2024-02-06 14:53:01 +0100

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,30 @@
+package PackageJustToTest;
+
+public class Point {
+        int x, y ;
+        String title ;
+
+        void setX(int x) {
+            this.x = x ;
+        }
+
+        void setY(int y) {
+            this.y = y ;
+        }
+
+        int getX() {
+            return 0;
+        }
+
+        int getY() {
+            return 0;
+        }
+
+        String getTitle(){
+            return title ;
+        }
+        void setTitle(String title){
+            this.title = title ;
+        }
+    }
+
